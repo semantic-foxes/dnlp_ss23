a358c153e249c3dfe9b23846a726d44fc2055743
Georgy Belousov <georgy.belousov@stud.uni-goettingen.de>
2023-09-01 13:58:30 +0200

    WIP refactored train_batch.py

--- src/core/train_epoch/train_batch.py
+++ src/core/train_epoch/train_batch.py
@@ -3,21 +3,26 @@ import torch
 from torch import nn
 
 
-def _batch_forward_sentiment(batch, model, device, criterion):
+def _batch_forward_sentiment(
+        batch: torch.tensor,
+        device: torch.device,
+        model: nn.Module,
+) -> torch.tensor:
     ids, attention_masks = batch['token_ids'], batch['attention_masks']
 
     ids = ids.to(device)
     attention_masks = attention_masks.to(device)
 
     predictions = model('sentiment', ids, attention_masks)
-
-    loss = criterion(predictions, batch['targets']).sum()
-    loss.backward()
-
     return predictions
 
 
-def _batch_forward_similarity_standard(batch, model, task, device, criterion):
+def _batch_forward_similarity(
+        batch: torch.tensor,
+        device: torch.device,
+        model: nn.Module,
+        task: str
+) -> torch.tensor:
     ids_1, attention_masks_1, ids_2, attention_masks_2 = \
         (batch['token_ids_1'], batch['attention_masks_1'],
          batch['token_ids_2'], batch['attention_masks_2'])
@@ -28,35 +33,14 @@ def _batch_forward_similarity_standard(batch, model, task, device, criterion):
     attention_masks_2 = attention_masks_2.to(device)
 
     predictions = model(task, ids_1, attention_masks_1, ids_2, attention_masks_2)
-
-    loss = criterion(predictions, batch['targets']).sum()
-    loss.backward()
-
     return predictions
 
 
-def _batch_forward_similarity_contrastive(batch_list, model, task, device, criterion):
-    for batch in batch_list:
-        ids_1, attention_masks_1, ids_2, attention_masks_2 = \
-            (batch['token_ids_1'], batch['attention_masks_1'],
-             batch['token_ids_2'], batch['attention_masks_2'])
-
-        ids_1 = ids_1.to(device)
-        ids_2 = ids_2.to(device)
-        attention_masks_1 = attention_masks_1.to(device)
-        attention_masks_2 = attention_masks_2.to(device)
-
-        predictions = model(task, ids_1, attention_masks_1, ids_2, attention_masks_2)
-
-        loss = criterion(predictions, batch['targets']).sum()
-        loss.backward()
-    # TODO: this is train only!
-    #return predictions
-
-
-def _batch_forward_similarity_triplet(batch, model, task, device):
-    # Should this accept task or not?
-    # Is this pretrain only?
+def _batch_forward_triplet(
+        batch: torch.tensor,
+        device: torch.device,
+        model: nn.Module,
+) -> tuple[torch.tensor, torch.tensor, torch.tensor]:
     (token_ids_anchor,
      attention_masks_anchor,
      token_ids_positive,
@@ -86,33 +70,80 @@ def _batch_forward_similarity_triplet(batch, model, task, device):
     embedding_anchor = model('embed', token_ids_anchor, attention_masks_anchor)
     embedding_positive = model('embed', token_ids_positive, attention_masks_positive)
     embedding_negative = model('embed', token_ids_negative, attention_masks_negative)
-    # TODO: cosine distance
-    # TODO: keep the criterion separately
-    criterion = torch.nn.TripletMarginLoss()
-    loss = criterion(embedding_anchor, embedding_positive, embedding_negative).sum()
-    loss.backward()
-    #return embedding_anchor, embedding_positive, embedding_negative
 
-# TODO: remove
-def _batch_forward(
+    return embedding_anchor, embedding_positive, embedding_negative
+
+def train_one_batch_standard(
         batch: torch.tensor,
-        model: nn.Module,
-        task: str,
-        device: torch.device,
         criterion: torch.nn.Module,
-        train_mode: str
+        device: torch.device,
+        model: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        task: str,
 ):
+    optimizer.zero_grad()
+
     if task == 'sentiment':
-        return _batch_forward_sentiment(batch, model, device, criterion)
+        predictions = _batch_forward_sentiment(batch, device, model)
     elif task == 'paraphrase_classifier' or task == 'paraphrase_regressor':
-        if train_mode == 'contrastive':
-            return _batch_forward_similarity_contrastive(batch, model, task, device, criterion)
-        elif train_mode == 'triplet':
-            return _batch_forward_similarity_triplet(batch, model, task, device)
-        else:
-            return _batch_forward_similarity_standard(batch, model, task, device, criterion)
+        predictions = _batch_forward_similarity(batch, device, model, task)
     else:
         raise NotImplementedError
+
+    targets = batch['targets'].to(device)
+
+    loss = criterion(predictions, targets).sum()
+    loss.backward()
+
+    optimizer.step()
+
+def train_one_batch_contrastive(
+        batch_list: list[torch.tensor],
+        criterion: torch.nn.Module,
+        device: torch.device,
+        model: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        task: str,
+        return_predictions: bool = False
+):
+    optimizer.zero_grad()
+    # TODO: weighted loss
+    if return_predictions:
+        original_batch = batch_list.pop(0)
+        original_predictions = _batch_forward_similarity(original_batch, device, model, task)
+        targets = batch['targets'].to(device)
+        loss = criterion(original_predictions, targets).sum()
+        loss.backward()
+
+    for batch in batch_list:
+        predictions = _batch_forward_similarity(batch, device, model, task)
+        targets = batch['targets'].to(device)
+        loss = criterion(predictions, targets).sum()
+        loss.backward()
+
+    optimizer.step()
+
+    if return_predictions:
+        return original_predictions
+
+
+def train_one_batch_triplet(
+        batch: torch.tensor,
+        criterion: torch.nn.Module,
+        device: torch.device,
+        model: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        task: str
+):
+    optimizer.zero_grad()
+
+    embeddings = _batch_forward_triplet(batch, device, model)
+
+    loss = criterion(*embeddings).sum()
+    loss.backward()
+
+    optimizer.step()
+
 
 def train_one_batch_multitask(
         model: nn.Module,
@@ -123,7 +154,20 @@ def train_one_batch_multitask(
         task: str,
         train_mode: str
 ):
-    optimizer.zero_grad()
-    # TODO: pass correct option instead of 'criterion'
-    _batch_forward(batch, model, task, device, criterion, train_mode)
-    optimizer.step()
+    if train_mode == 'contrastive':
+        train_one_batch_contrastive(batch, criterion, device, model, optimizer, task)
+    elif train_mode == 'triplet':
+        forced_criterion = torch.nn.TripletMarginLoss
+        train_one_batch_triplet(batch, forced_criterion, device, model, optimizer, task)
+    else:
+        train_one_batch_standard(batch, criterion, device, model, optimizer, task)
+
+# TODO: remove
+def _batch_forward(
+        batch: torch.tensor,
+        model: nn.Module,
+        task: str,
+        device: torch.device,
+        criterion: torch.nn.Module
+):
+    raise NotImplementedError("Use train_one_batch_multitask instead of calling _batch_forward directly.")
